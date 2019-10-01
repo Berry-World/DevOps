a1c3c414b3a8622f6da455508818d2ec8ce31f09
Farzad Jalali <Farzad-Jalali@users.noreply.github.com>
2019-10-01 10:36:14 +0100

    Update build-script.ps1

--- AKS/V1/build-script.ps1
+++ AKS/V1/build-script.ps1
@@ -435,3 +435,12 @@ else
     
 }
 
+
+if ($routeChanging  -eq $true) {
+      
+  $newRouteValue = '[Route("' + $app + $namespace + '/api'
+  $routeReplacingHashTable =  @{ '\[Route\(\"api' = '[Route("tst1/finance/api'  }
+
+  & ($routeFixerPath) -routeChanging $routeChanging -routrFilePath $routrFilePath -RoutFilefilter $RoutFilefilter -routeReplacingHashTable $routeReplacingHashTable  
+}
+
